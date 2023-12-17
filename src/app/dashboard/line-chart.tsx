4192b055a6a556cb10b91c6d8b065118036dee30
ibrokhim <mr017programmer@gmail.com>
2023-12-17 16:37:51 +0500

    File Colocation

---
+++ src/app/dashboard/line-chart.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const LineChart = (props: Props) => {
+  return (
+    <div>
+      <h2>LineChart</h2>
+    </div>
+  );
+};
+
+export default LineChart;
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type Props = {};
+
+const Dashboard = (props: Props) => {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+};
+
+export default Dashboard;
+
+function BarChart() {
+  return (
+    <div>
+      <h2>BarChart</h2>
+    </div>
+  );
+}
