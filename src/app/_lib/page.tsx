0ee5d5037d81fa4d362bd4fabd10b829946caa91
ibrokhim <mr017programmer@gmail.com>
2023-12-17 16:44:22 +0500

    Private Folders

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const PrimavteRoute = (props: Props) => {
+  return (
+    <div>
+      <h2>PrimavteRoute</h2>
+    </div>
+  );
+};
+
+export default PrimavteRoute;
