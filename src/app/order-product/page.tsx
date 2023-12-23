6eeffc5f5c9943b4efb4d8fbb6d191ef5a443a78
ibrokhim <mr017programmer@gmail.com>
2023-12-23 15:39:59 +0500

    Navigating Programmatically

--- src/app/order-product/page.tsx
+++ src/app/order-product/page.tsx
@@ -7,7 +7,7 @@ export default function page() {
 
 	return (
 		<div>
-			<h1 className="mb-10">OrderProduct</h1>
+			<h1 className="mb-10">Order Product</h1>
 
 			<div className="flex gap-5">
 				<button className="bg-orange-100 hover:bg-orange-200 transition-all duration-300 border p-2 rounded-xl" onClick={() => router.push("/")}>
@@ -17,7 +17,7 @@ export default function page() {
 					Replace to Products
 				</button>
 				<button className="bg-orange-100 hover:bg-orange-200 transition-all duration-300 border p-2 rounded-xl" onClick={() => router.back()}>
-					goBack
+					GoBack
 				</button>
 			</div>
 		</div>
