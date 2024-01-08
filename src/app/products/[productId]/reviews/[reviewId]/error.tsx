dd498d58a1efd66ea4404dfe1c93eb3f73d7b7b9
ibrokhim <mr017programmer@gmail.com>
2024-01-08 11:21:00 +0500

    25-RecoveringErrors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -2,13 +2,17 @@
 
 interface IProps {
 	error: Error;
+	reset: VoidFunction;
 }
 
-const ErrorBoundary = ({ error }: IProps) => {
+const ErrorBoundary = ({ error, reset }: IProps) => {
 	return (
 		<section>
 			<h1 className="text-xl weight-700">Error Boundary</h1>
 			<pre>{JSON.stringify(error.message, null, 2)}</pre>
+			<button className="px-5 py-3 border border-gray rounded" onClick={reset}>
+				Try Again
+			</button>
 		</section>
 	);
 };
